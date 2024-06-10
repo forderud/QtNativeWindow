1438592b9253f9802f81165b2ebaf1462d4dd59a
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-06-10 13:02:27 +0200

    Gradient background to test native window transparency handling

--- Main.cpp
+++ Main.cpp
@@ -42,8 +42,11 @@ int main(int argc, char *argv[]) {
         QWidget* mainWidget = new QWidget;
         mainWidget->setLayout(layout);
         win.setCentralWidget(mainWidget);
+
+        // gradient background to test native window transparency
+        mainWidget->setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255))");
     }
-    
+
     win.show();
     return a.exec();
 }
