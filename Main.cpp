cdbc2498a32bb6224141473a5d2ce77371c6e07d
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-06-10 13:10:51 +0200

    Refactor more.

--- Main.cpp
+++ Main.cpp
@@ -7,21 +7,28 @@
 #include "TransparentWindow.hpp"
 
 
-class MyWindow : public QMainWindow {
+/** Sample Qt window with native window UI. */
+class QtWindowWithForeigContent : public QMainWindow {
 public:
-    MyWindow() {
-        m_layout = new QHBoxLayout;
-
+    QtWindowWithForeigContent() {
         QWidget* mainWidget = new QWidget;
-        mainWidget->setLayout(m_layout);
         setCentralWidget(mainWidget);
 
         // gradient background to test native window transparency
         mainWidget->setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255))");
+
+        m_layout = new QHBoxLayout;
+        mainWidget->setLayout(m_layout);
     }
 
-    void Add(QWidget* widget) {
-        m_layout->addWidget(widget);
+    void AddNativeWindow(HWND nativeHandle) {
+        // wrap native window handle
+        QWindow* window = QWindow::fromWinId((WId)nativeHandle);
+
+        // re-parent native window
+        QWidget* container = QWidget::createWindowContainer(window);
+
+        m_layout->addWidget(container);
     }
 
 private:
@@ -29,30 +36,19 @@ private:
 };
 
 
-static QWidget* EmbedNativeWindow(HWND nativeHandle) {
-    // wrap native window handle
-    QWindow* window = QWindow::fromWinId((WId)nativeHandle);
-
-    // re-parent native window
-    QWidget* container = QWidget::createWindowContainer(window);
-    return container;
-}
-
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    MyWindow win;
+    QtWindowWithForeigContent win;
 
     // create native windows
     LayeredWindow lw;
     lw.Create(reinterpret_cast<HWND>(win.winId())); // parent window handle from Qt
+    win.AddNativeWindow(lw.m_hWnd); // add to Qt window
 
     TransparentWindow tw;
     tw.Create(reinterpret_cast<HWND>(win.winId())); // parent window handle from Qt
-
-    // embed native widows in Qt window
-    win.Add(EmbedNativeWindow(lw.m_hWnd));
-    win.Add(EmbedNativeWindow(tw.m_hWnd));
+    win.AddNativeWindow(tw.m_hWnd); // add to Qt window
 
     win.show();
     return a.exec();
