ad4c4e6eef8f487300972d665ae3393a0cc31d85
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-06-10 12:42:10 +0200

    Working baseline.

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,38 @@
 #include <QApplication>
+#include <QMainWindow>
+#include <QWindow>
+#include <QWidget>
 #include "LayeredWindow.hpp"
 #include "TransparentWindow.hpp"
 
 
+class MyWindow : public QMainWindow {
+public:
+};
+
+
+static QWidget* EmbedNativeWindow(HWND nativeHandle) {
+    // wrap native window handle
+    QWindow* window = QWindow::fromWinId((WId)nativeHandle);
+
+    // re-parent native window
+    QWidget* container = QWidget::createWindowContainer(window);
+    return container;
+}
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    MyWindow win;
+
+    // create native window
+    HWND parent = reinterpret_cast<HWND>(win.winId());
+    LayeredWindow nw;
+    nw.Create(parent);
+
+    // embed native widow in Qt window
+    win.setCentralWidget(EmbedNativeWindow(nw.m_hWnd));
+    
+    win.show();
     return a.exec();
 }
