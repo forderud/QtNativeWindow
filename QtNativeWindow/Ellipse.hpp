71f72e76b944b3871f011ce9ae7a0ff7a859de39
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-08-20 10:46:49 +0200

    Simplify logic with CRect.

--- QtNativeWindow/Ellipse.hpp
+++ QtNativeWindow/Ellipse.hpp
@@ -14,11 +14,10 @@ RGBQUAD RGBAPremult(BYTE r, BYTE g, BYTE b, BYTE a) {
 }
 
 /** Check if a coordinate is inside an ellipse. */
-static bool IsInsideEllipse(POINT pt, RECT ellipse) {
-    POINT center{ (ellipse.right + ellipse.left) / 2, (ellipse.bottom + ellipse.top) / 2 }; // ellipse center
-    POINT ext{ (ellipse.right - ellipse.left) / 2, (ellipse.bottom - ellipse.top) / 2 }; // ellipse extends
+static bool IsInsideEllipse(CPoint pt, CRect ellipse) {
+    CPoint ext{ (ellipse.right - ellipse.left) / 2, (ellipse.bottom - ellipse.top) / 2 }; // ellipse extends
 
-    POINT rel{ pt.x - center.x, pt.y - center.y }; // point relative to center
+    CPoint rel = pt - ellipse.CenterPoint(); // point relative to center
 
     // ellipse formula
     return (rel.x * rel.x) * 1.0f / (ext.x * ext.x) + (rel.y * rel.y) * 1.0f / (ext.y * ext.y) <= 1.0f;
